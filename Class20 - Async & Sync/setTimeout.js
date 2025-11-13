c88c83b1b13a82dcd3390af41e6ee55a90d73b88
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-13 10:58:30 +0530

    Async, Await & Callback

---
+++ Class20 - Async & Sync/setTimeout.js
@@ -0,0 +1,21 @@
+setTimeout(()=>{
+    console.log("R1")
+    
+},8000)
+
+
+setTimeout(()=>{
+    console.log("R2")
+    
+},6000)
+
+
+setTimeout(()=>{
+    console.log("R3")
+    
+},4000)
+
+setTimeout(()=>{
+    console.log("R4")
+    
+},1000)
