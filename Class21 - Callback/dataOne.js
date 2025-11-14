36bb07b02970e36a0981ce0b278c437dccad1c31
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-14 11:15:34 +0530

    Callback

---
+++ Class21 - Callback/dataOne.js
@@ -0,0 +1,31 @@
+//Using CALLBACK
+
+let employees = [
+    {eid:101,ename:"Rahul",esal:45000},
+    {eid:102,ename:"Sonia",esal:55000}
+]
+
+let createEmployee = (emp,callback)=>{
+    setTimeout(()=>{
+        employees.push(emp)
+        callback()
+    },4000)
+}
+
+let getEmployees = ()=>{
+    setTimeout(()=>{
+        let rows = ""
+        for (let emp of employees){
+            rows = rows + `
+            <tr>
+                <td>${emp.eid}</td>
+                <td>${emp.ename}</td>
+                <td>${emp.esal}</td>
+            </tr>`
+        }
+
+    document.getElementById('tb_data').innerHTML = rows
+    },1000)
+}
+
+createEmployee({eid:103,ename:"Priya",esal:65000},getEmployees)
