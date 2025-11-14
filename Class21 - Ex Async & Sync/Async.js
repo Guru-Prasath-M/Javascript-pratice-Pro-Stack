e5a5deb1dc1a1c29b51b254b388f45859e70e5cd
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-14 10:21:44 +0530

    Ex for Async & Sync

---
+++ Class21 - Ex Async & Sync/Async.js
@@ -0,0 +1,6 @@
+//Asynchronous
+
+setTimeout(()=>{console.log("R1")}, 5000)
+setTimeout(()=>{console.log("R2")}, 4000)
+setTimeout(()=>{console.log("R3")}, 3000)
+setTimeout(()=>{console.log("R4")}, 2000)
---
+++ Class21 - Ex Async & Sync/Sync.js
@@ -0,0 +1,6 @@
+//Synchronous
+
+console.log("R1"),5000
+console.log("R2"),4000
+console.log("R3"),3000
+console.log("R4"),2000
