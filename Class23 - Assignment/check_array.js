b35b90620e0252c5f932d5fee8317e9da674bdac
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-18 10:52:37 +0530

    Array is empty or not

---
+++ Class23 - Assignment/check_array.js
@@ -0,0 +1,19 @@
+let nums = []
+let user = [
+    {},
+    {}
+]
+
+if(nums.length > 0){
+    console.log("Array is not empty");
+}else{
+    console.log("Array is empty");
+    
+}
+
+if(user.length > 0){
+    console.log("Array is not empty");
+}else{
+    console.log("Array is empty");
+    
+}
