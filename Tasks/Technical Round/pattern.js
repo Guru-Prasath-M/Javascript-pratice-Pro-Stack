06a3a433236ce36ca8f9861e44f622af91cc3dbb
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-25 15:59:49 +0530

    Pattern's

---
+++ Tasks/Technical Round/pattern.js
@@ -0,0 +1,17 @@
+// Printing Pattern
+// * * * * * 
+// * * * * * 
+// * * * * *
+// * * * * *
+// * * * * *
+
+let n = 5;
+for(let i = 1;i<=n;i++){
+let row = ''
+    for(j=1;j<=n;j++){
+        row = row + "* "
+
+    }
+    console.log(row);
+    
+}
---
+++ Tasks/Technical Round/pattern3.js
@@ -0,0 +1,15 @@
+// Printing Pattern
+// * * * * * 
+// * * * * 
+// * * *
+// * *
+// *
+
+let n = 5;
+for(let i=5 ; i>=1 ; i--){
+    let row = ""
+    for(let j=1; j<=i ; j++){
+        row = row+ "* "
+    }
+    console.log(row);
+}
